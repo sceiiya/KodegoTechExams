013b8f98426b9d82c90a738f1e61da0ccadb4db0
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-07-03 00:08:05 +0800

    finish closest to zero

--- SalesProblem.js
+++ SalesProblem.js
@@ -1,12 +1,3 @@
-
-const productProfitArray = [
-    {"Product A": -22},
-    {"Product B": 35},
-    {"Product C": -81},
-    {"Product D": -1},
-    {"Product E": -75},
-];
-
 function pushProfits(data){
     //instantiate profit variable
     let profits = [];
@@ -19,11 +10,19 @@ function pushProfits(data){
     return profits;
 }
 
+function pushAbsProfits(data){
+    let profits = [];
+
+    data.forEach(product => {
+        const profit = Math.abs(Object.values(product)[0]);
+        profits.push(profit);
+    });    
+    return profits;
+}
+
 function pushProducts(data){
-    //instantiate profit variable
     let productsName = [];
 
-    //store each value of the key in arry of objects
     data.forEach(product => {
         const name = Object.keys(product)[0];
         productsName.push(name);
@@ -67,8 +66,32 @@ function bottomProduct(data){
     }
 }
 
+function zeroProfitProduct(data){
+    if (data.length === 0) {
+        return 'No Data';
+    } else if (data === ''){
+        return 'No Data';
+    }else{
+        let profits = pushAbsProfits(data);
+        let names = pushProducts(data);
 
+        let closestToZero = Math.min(...profits);
+        
+        let pos = data.findIndex(data => Math.abs(Object.values(data)[0]) === closestToZero);
+        
+        return names[pos];
+    }
+}
 
-// topProduct(productProfitArray) // 35
+//Initialize Data
+const productProfitArray = [
+    {"Product A": -22},
+    {"Product B": 35},
+    {"Product C": -81},
+    {"Product D": -1},
+    {"Product E": -75},
+];
+
 console.log(topProduct(productProfitArray)); //Product B
 console.log(bottomProduct(productProfitArray)); //Product C
+console.log(zeroProfitProduct(productProfitArray)); //Product D
