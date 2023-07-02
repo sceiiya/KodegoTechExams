2b6e206fbca422e480efbf6b4dcdd93fb2acdabb
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-07-03 05:15:45 +0800

    finish longest dynasty

--- LongestDynasty.js
+++ LongestDynasty.js
@@ -1,3 +1,91 @@
+
+// convert the roman letters into number value
+const numericize = (roman) => {
+    roman = roman.toUpperCase();
+    let numericized = 0;
+
+    switch (roman) {
+        case 'M':
+            numericized = 1000;
+            break;
+        case 'D':
+            numericized = 500;
+            break;
+        case 'C':
+            numericized = 100;
+            break;
+        case 'L':
+            numericized = 50;
+            break;
+        case 'X':
+            numericized = 10;
+            break;
+        case 'V':
+            numericized = 5;
+            break;
+        case 'I':
+            numericized = 1;
+            break;
+        default:
+            return undefined;
+            break;
+    }
+
+    return numericized;
+}
+
+function convertYear(year){
+    if(year === NaN){
+        return 'undefined';
+    }
+
+    let numerical = 0;
+
+    for (let i = 0; i < year.length; i++) {
+        let roman = numericize(year[i]);
+        if (roman == undefined){
+            return 'Invalid';
+        }
+        numerical += roman;
+    }
+    return numerical;
+
+}
+
+function longestDynasty(data){
+    if (data.length === 0) {
+        return "No Data";
+    }
+    const allYears = [];
+    const reignYears = [];
+    const allDynasty = [];
+
+    // identify element and store in an array
+    data.forEach((datayear, i) => {
+        const dynasty = Object.keys(datayear)[0];
+        allDynasty.push(dynasty)
+
+        const roman = Object.values(datayear)[0];
+        let year = convertYear(roman);
+        allYears.push(year);
+        let reign = 0;
+        if(i == 0){
+            reign = year - 1000;
+        }else{
+            reign = year - allYears[i- 1]
+        }
+        reignYears.push(reign)
+    });
+
+    const longest = Math.max(...reignYears);
+    const indexOfLongest = reignYears.indexOf(longest);
+    const longestDynasty = allDynasty[indexOfLongest];
+
+    return longestDynasty;
+    // return reignYears;
+}
+
+//initialize data
 const dynastyReign = [
     {"San Dynasty": "MXLI"},
     {"Viloria Dynasty": "MCCCIIII"},
@@ -8,33 +96,24 @@ const dynastyReign = [
     {"Andre Dynasty": "MMMXICX"},
 ];
 
-const rule = [
-    {"I": 1},
-    {"V": 5},
-    {"X": 10},
-    {"L": 50},
-    {"C": 100},
-    {"D": 500},
-    {"M": 1000},
-];
+// years converted from roman values
+// San Dynasty: 1061
+// Viloria Dynasty: 1304
+// Tan Dynasty: 1418
+// Bon Dynasty: 1665
+// Maiko Dynasty: 1666
+// Paul Dynasty: 2171
+// Andre Dynasty: 3121
 
-function longestDynasty(data){
-    //code
-    //call convert year
-    convertYear(datayear)
-}
-
-function convertYear(data){
-    //insert validation
-    //code
-    if (condition) {
-        return 'Invalid';
-    }else if (data === '' || data === null){
-        return 'No Data';
-    } else {
-        
-    }
-}
-
-longestDynasty(dynastyReign);
-console.log(longestDynasty(dynastyReign));
+//reign years converted values
+// San Dynasty: 61
+// Viloria Dynasty: 243
+// Tan Dynasty: 114
+// Bon Dynasty: 247
+// Maiko Dynasty: 1
+// Paul Dynasty: 505
+// Andre Dynasty: 950
+  
+// longestDynasty(dynastyReign);
+console.log(longestDynasty(dynastyReign)); // Andre Dynasty = 950 years
+// console.log(convertYear('Mdasfe'))
