4d0e090d0927ba7864ccf7d1a1c918b13ec26c3a
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-07-11 01:49:18 +0800

    identify identifyiers

---
+++ HTML-CSS-JS_PE/index.js
@@ -0,0 +1,6 @@
+const headline1 = document.querySelector('.headline1');
+const headline2 = document.querySelector('.headline2');
+const subheadline = document.querySelector('.subheadline');
+const learn_more = document.querySelector('.learn-more');
+const logo = document.querySelector('.logo');
+const replay_button = document.querySelector('.replay-button');
